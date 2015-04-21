c2bb8292fb4eee7be3447fd691964dd06c8a6ff4
Cedric <cliengme@gmail.com>
2015-04-21 13:50:58 +0200

    logo and splash

--- www/js/app.js
+++ www/js/app.js
@@ -5,15 +5,19 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('hydromerta', ['ionic', 'hydromerta.constants', 'hydromerta.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+        .config(['$ionicConfigProvider', function ($ionicConfigProvider) {
+                $ionicConfigProvider.navBar.alignTitle('center');
+            }])
+
+        .run(function ($ionicPlatform) {
+            $ionicPlatform.ready(function () {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        })
