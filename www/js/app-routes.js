854fa3ae29fe8957a67b6a4781ca7ac4613affc8
quentindelattre <hey@quentindelattre.ch>
2015-04-28 09:45:24 +0200

    Template action detail vide

--- www/js/app-routes.js
+++ www/js/app-routes.js
@@ -13,6 +13,13 @@ angular.module('hydromerta')
                         templateUrl: 'templates/register.html',
                         controller: 'loginController'
                     })
+                    
+
+                    .state('actionDetail', {
+                        url: '/action',
+                        templateUrl: 'templates/actionDetail.html',
+                        controller: 'actionController'
+                    })
             $urlRouterProvider.otherwise(function ($injector) {
                 $injector.get('$state').go('map'); // Go to the new issue tab by default.
             });
